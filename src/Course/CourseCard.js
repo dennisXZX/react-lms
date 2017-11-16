99b59683e3fd98d35602990d4df0d3c43b308b54
Dennis Xiao <dennisboys@gmail.com>
2017-11-16 15:30:37 +1100

    styled course card

--- src/Course/CourseCard.js
+++ src/Course/CourseCard.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import { truncate } from '../utils';
 
-export default function CourseCard({ course }) {
-  return (
-    <div className="col-sm-6 col-md-3">
-      <div className="thumbnail course-card">
-        <div className="caption">
-          <h4 className="course-card-name">{truncate(course.name, 20)}</h4>
-          <p className="course-card-code">{truncate(course.code, 30)}</p>
-          <p className="course-card-introduction">{truncate(course.introduction, 60)}</p>
-          <p>
-            <Link to={`/courses/${course.id}`} className="btn btn-default">
-              Details
-            </Link>
-          </p>
-        </div>
+class CourseCard extends Component {
+
+  // generate a random color
+  generateRandomColor = () => {
+    const colorLetters = ["B", "C", "D", "E", "F"];
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += colorLetters[Math.floor(Math.random() * colorLetters.length)];
+    }
+    return color;
+  }
+
+  render() {
+    const course = this.props.course;
+    const randomColor = this.generateRandomColor();
+
+    return (
+      <div className="col-sm-6 col-md-3">
+        <Link to={`/courses/${course.id}`}>
+          <div className="thumbnail course-card" style={{ backgroundColor: randomColor }}>
+            <div className="caption">
+              <h4 className="course-card-name">{truncate(course.name, 20)}</h4>
+              <p className="course-card-code">{truncate(course.code, 30)}</p>
+              <p className="course-card-introduction">{truncate(course.introduction, 60)}</p>
+            </div>
+          </div>
+        </Link>
       </div>
-    </div>
-  );
+    )
+  }
 }
+
+export default CourseCard;
+
+//
+// export default function CourseCard({ course }) {
+//   return (
+//     <div className="col-sm-6 col-md-3">
+//       <div className="thumbnail course-card">
+//         <div className="caption">
+//           <h4 className="course-card-name">{truncate(course.name, 20)}</h4>
+//           <p className="course-card-code">{truncate(course.code, 30)}</p>
+//           <p className="course-card-introduction">{truncate(course.introduction, 60)}</p>
+//           <p>
+//             <Link to={`/courses/${course.id}`} className="btn btn-default">
+//               Details
+//             </Link>
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
