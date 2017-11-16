37ea79d73405b433391e55ee6e5458e195b9f846
Dennis Xiao <dennisboys@gmail.com>
2017-11-16 13:13:01 +1100

    set up the project folder structure

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -4,11 +4,19 @@ import { Route, Switch } from 'react-router-dom';
 import WelcomeView from '../Home/WelcomeView';
 // import CourseDetailsView from '../Course/CourseDetailsView';
 // import CoursesView from '../Course/CoursesView';
+// import LecturerDetailsView from '../Lecturer/LecturerDetailsView';
+// import LecturersView from '../Lecturer/LecturersView';
+// import StudentDetailsView from '../Student/StudentDetailsView';
+// import StudentsView from '../Student/StudentsView';
 
 export default () => ((
   <Switch>
     <Route exact path="/" component={WelcomeView} />
     {/*<Route exact path="/courses" component={CoursesView} />*/}
     {/*<Route exact path="/courses/:id" component={CourseDetailsView} />*/}
+    {/*<Route exact path="/students" component={StudentsView} />*/}
+    {/*<Route exact path="/students/:id" component={StudentDetailsView} />*/}
+    {/*<Route exact path="/lecturers" component={LecturersView} />*/}
+    {/*<Route exact path="/lecturers/:id" component={LecturerDetailsView} />    */}
   </Switch>
 ));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+// libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import axios from 'axios';
 
+// CSS
 import './styles';
 
+// React component
 import Shell from './App/Shell';
 
+// employ service worker to make a progressive web app
 import registerServiceWorker from './registerServiceWorker';
 
 // set a default URL that applies to every request
