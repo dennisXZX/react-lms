b9e815537eb311aec8b480ea823e5a2dc716d667
Dennis Xiao <dennisboys@gmail.com>
2017-11-16 13:19:06 +1100

    fixed top nav bug

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import WelcomeView from '../Home/WelcomeView';
+import CoursesView from '../Course/CoursesView';
 // import CourseDetailsView from '../Course/CourseDetailsView';
-// import CoursesView from '../Course/CoursesView';
-// import LecturerDetailsView from '../Lecturer/LecturerDetailsView';
-// import LecturersView from '../Lecturer/LecturersView';
-// import StudentDetailsView from '../Student/StudentDetailsView';
 // import StudentsView from '../Student/StudentsView';
+// import StudentDetailsView from '../Student/StudentDetailsView';
+// import LecturersView from '../Lecturer/LecturersView';
+// import LecturerDetailsView from '../Lecturer/LecturerDetailsView';
 
 export default () => ((
   <Switch>
     <Route exact path="/" component={WelcomeView} />
-    {/*<Route exact path="/courses" component={CoursesView} />*/}
+    <Route exact path="/courses" component={CoursesView} />
     {/*<Route exact path="/courses/:id" component={CourseDetailsView} />*/}
     {/*<Route exact path="/students" component={StudentsView} />*/}
     {/*<Route exact path="/students/:id" component={StudentDetailsView} />*/}
---
+++ src/Course/CoursesView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CoursesView extends Component {
+  render() {
+    return (
+      <div>course</div>
+    )
+  }
+}
+
+export default CoursesView;
