6be252b55f29fa8c7b012a74e84e4b31c9368ee5
Dennis Xiao <dennisboys@gmail.com>
2017-12-03 15:56:00 +1100

    changed Bootstrap theme

--- src/components/Home/WelcomeView.js
+++ src/components/Home/WelcomeView.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import { Pie } from 'react-chartjs-2';
 
 class WelcomeView extends Component {
+   data = {
+    datasets: [{
+      data: [40, 20]
+    }],
+    labels: [
+      'Male',
+      'Female'
+    ],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)'
+    ],
+     borderColor: [
+      'rgba(255,99,132,1)',
+      'rgba(54, 162, 235, 1)'
+     ],
+     borderWidth: 1
+  };
 
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
         <div className="row chart">
-          <div className="col-sm-4 text-center">.col-md-4</div>
-          <div className="col-sm-4 text-center">.col-md-4</div>
-          <div className="col-sm-4 text-center">.col-md-4</div>
+          <div className="col-sm-6 text-center">
+            <Pie
+              data={this.data}
+              width={300}
+              height={300}
+              options={{
+                maintainAspectRatio: false
+              }} />
+          </div>
+          <div className="col-sm-6 text-center">.col-md-6</div>
         </div>
       </div>
     );
